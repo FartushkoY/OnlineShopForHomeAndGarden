ae64d1432c2916e7e0a634a0a20afabc0f8c0b21
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-08-07 22:18:11 +0200

    Initial commit

---
+++ src/main/java/de/telran/onlineshopforhomeandgarden1/OnlineShopForHomeAndGarden1Application.java
@@ -0,0 +1,13 @@
+package de.telran.onlineshopforhomeandgarden1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopForHomeAndGarden1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopForHomeAndGarden1Application.class, args);
+    }
+
+}
