1ea8e0d672fe66073ad969c7f151a8a3b07dcf5c
VoytovychM <mary.voytovych@gmail.com>
2024-08-09 13:38:51 +0200

    [SCRUM14] Connection Scheme to DB

--- src/main/resources/db/migrations/002_data.sql
+++ src/main/resources/db/migrations/002_data.sql
@@ -17,24 +17,23 @@ values  (1, 1,'Tulip', 'Elevate your garden with our exquisite Tulip planting ma
         (2, 6,'Horticultural Charcoal', 'Enhance your gardening experience with Horticultural Charcoal. This natural product purifies soil, prevents root rot, and promotes plant health. Elevate your garden\'s resilience.', 13.00, 15.00, 'https://raw.githubusercontent.com/tel-ran-de/telran_backend_garden_shop/master/public/product_img/28.jpeg', current_timestamp(), current_timestamp()),
         (3, 7, 'Organic Perlite', 'Fuel your plants\' growth with Organic Perlite. This lightweight, porous medium enhances aeration and drainage, ensuring optimal conditions for your garden. Choose organic vitality.', 6.00, 0.00, 'https://raw.githubusercontent.com/tel-ran-de/telran_backend_garden_shop/master/public/product_img/24.jpeg', current_timestamp(), current_timestamp()),
         (3, 8, 'Ocean Forest', 'Dive into luNourish your plants with our Potting Mix fertilizer. Perfectly blended for optimal growth, it provides the essential nutrients your plants crave. Cultivate a flourishing garden today.', 7.00, 0.00, 'https://raw.githubusercontent.com/tel-ran-de/telran_backend_garden_shop/master/public/product_img/27.jpeg', current_timestamp(), current_timestamp()),
-        (3, 9, 'Potting Mix', 'Nourish your plants with our Potting Mix fertilizer. Perfectly blended for optimal growth, it provides the essential nutrients your plants crave. Cultivate a flourishing garden today.', 8.50, 0.00, 'https://raw.githubusercontent.com/tel-ran-de/telran_backend_garden_shop/master/public/product_img/23.jpeg', current_timestamp(), current_timestamp());
+        (3, 9, 'Potting Mix', 'Nourish your plants with our Potting Mix fertilizer. Perfectly blended for optimal growth, it provides the essential nutrients your plants crave. Cultivate a flourishing garden today.', 8.50, 0.00, 'https://raw.githubusercontent.com/tel-ran-de/telran_backend_garden_shop/master/public/product_img/23.jpeg', current_timestamp(), current_timestamp()),
         (4, 10, 'Greek Pot', 'Add a touch of ancient elegance with our Greek Pot. Crafted for both style and durability, this planter elevates your plants, turning your garden into a timeless sanctuary.', 30.00, 10.00, 'https://raw.githubusercontent.com/tel-ran-de/telran_backend_garden_shop/master/public/product_img/1.jpeg', current_timestamp(), current_timestamp()),
         (4, 11,'Wicker Pot', 'Embrace natural aesthetics with our Wicker Pot. Stylish and functional, it complements any garden space. Elevate your plant display with this charming wicker planter.', 20.00, 0.00, 'https://raw.githubusercontent.com/tel-ran-de/telran_backend_garden_shop/master/public/product_img/12.jpeg', current_timestamp(), current_timestamp()),
         (4, 12,'Red Pot', 'Make a bold statement in your garden with our vibrant Red Pot. Durable and eye-catching, it adds a pop of color to your green haven. Shop now for a standout planter.', 25.00, 0.00, 'https://raw.githubusercontent.com/tel-ran-de/telran_backend_garden_shop/master/public/product_img/10.jpeg', current_timestamp(), current_timestamp()),
-        (5, 13, 'Shovel', 'Dig into gardening with our sturdy Shovel. Designed for durability and comfort, it\'s an essential tool for planting and landscaping. Elevate your gardening experience with quality tools.', 40.00, 20.00, 'https://assets.leevalley.com/Size4/10115/PG107-radius-ergonomic-stainless-steel-shovel-u-0195.jpg', current_timestamp(), current_timestamp()),
+        (5, 13, 'Shovel', 'Dig into gardening with our sturdy Shovel. Designed for durability and comfort, it is an essential tool for planting and landscaping. Elevate your gardening experience with quality tools.', 40.00, 20.00, 'https://assets.leevalley.com/Size4/10115/PG107-radius-ergonomic-stainless-steel-shovel-u-0195.jpg', current_timestamp(), current_timestamp()),
         (5, 14,'Rake', 'Maintain a pristine garden with our reliable Rake. Perfect for leaf and debris removal, it ensures a tidy outdoor space. Upgrade your gardening arsenal with this essential tool.', 38.00, 0.00, 'https://images.ctfassets.net/zma7thmmcinb/46JNtlvxFdhCD2XPHHziLc/31fe4425eff26086a7eb884a4384d85b/find-the-right-rake-plastic-rake.jpg', current_timestamp(), current_timestamp()),
         (5, 15,'Gardening scissors', 'Precision meets functionality with our Gardening Scissors. Trim and shape your garden with ease. Elevate your gardening skills with these sharp and durable scissors.', 20.00, 0.00, 'https://cdn.thewirecutter.com/wp-content/uploads/2015/06/pruningshears-2x1-.jpg?auto=webp&quality=75&crop=2:1&width=1024&dpr=2', current_timestamp(), current_timestamp());
 
-insert into users (user_id, name, email, phone_number, password, role)
-values (1, 'John Doe', 'john.doe@example.com', '+49 170 8009466', 'strawberry_123', 'CUSTOMER'),
-       (2, 'Jane Smith','jane.smith@example.com', '+49 166 8019467','raspberry_456', 'CUSTOMER'),
-       (3, 'Alice Jones','alice.jones@example.com', '+49 167 8019468', 'blueberry_789',  'CUSTOMER'),
-       (4, 'Masha Voytovych', 'mary.voytovych@gmail.com', '+49 168 8019469' 'tomato_113', 'ADMINISTRATOR'),
-       (5, 'Iryna Kosiakovska', 'iryna@example.com', '+49 165 8019460' 'potato_112', 'ADMINISTRATOR'),
-       (6, 'Yevgenia Fartushko','yevgenia@example.com', '+49 164 8019471' 'cucumber_111', 'ADMINISTRATOR'),
-       (7, 'Michael Brown', 'michael.brown@example.com', '+49 162 8029468', 'Tomato_789', 'CUSTOMER'),
-       (8, 'Emily Davis', 'emily.davis@example.com', '+49 160 8039469', 'blacKberry_101', 'CUSTOMER');
-
+insert into users (user_id, name, email, phone_number, password_hash, role)
+values (1, 'John Doe', 'john.doe@example.com', '+491708009466', 'strawberry_123', 'CUSTOMER'),
+       (2, 'Jane Smith','jane.smith@example.com', '+491668019467','raspberry_456', 'CUSTOMER'),
+       (3, 'Alice Jones','alice.jones@example.com', '+491678019468', 'blueberry_789',  'CUSTOMER'),
+       (4, 'Masha Voytovych', 'mary.voytovych@gmail.com', '+491688019469', 'tomato_113', 'ADMINISTRATOR'),
+       (5, 'Iryna Kosiakovska', 'iryna@example.com', '+491658019460', 'potato_112', 'ADMINISTRATOR'),
+       (6, 'Yevgenia Fartushko','yevgenia@example.com', '+491648019471', 'cucumber_111', 'ADMINISTRATOR'),
+       (7, 'Michael Brown', 'michael.brown@example.com', '+491628029468', 'Tomato_789', 'CUSTOMER'),
+       (8, 'Emily Davis', 'emily.davis@example.com', '+491608039469', 'blacKberry_101', 'CUSTOMER');
 
 insert into cart (cart_id, user_id)
 values (1, 1),
@@ -56,11 +55,11 @@ values (1, 2, 5),
        (3, 3, 14);
 
 insert into orders (order_id, user_id, created_at, delivery_address, contact_phone, delivery_method, status, updated_at)
-values (1, 2, current_timestamp(), 'Friedrichstraße 123, 10117, Berlin, Germany', '+49 166 8019467', 'STANDARD', 'PAID', current_timestamp()),
-       (2, 1, current_timestamp(), 'Kurfürstendamm 45, 10719, Berlin, Germany', '+49 170 8009466', 'EXPRESS', 'DELIVERED', current_timestamp()),
-       (3, 3, current_timestamp(), 'Karl-Marx-Allee 50, 10243, Berlin, Germany', '+49 167 8019468', 'STANDARD', 'CANCELED', current_timestamp()),
-       (4, 7, current_timestamp(), 'Schlossstasse 45, 10719, Berlin, Germany', '+49 170 7709466', 'STANDARD', 'ON THE WAY', current_timestamp()),
-       (5, 8, current_timestamp(), 'Karslruherstrasse 11, 10243, Schwetzingen, Germany', '+49 167 8044468', 'EXPRESS', 'PENDING', current_timestamp());
+values (1, 2, current_timestamp(), 'Friedrichstraße 123, 10117, Berlin, Germany', '+491668019467', 'STANDARD', 'PAID', current_timestamp()),
+       (2, 1, current_timestamp(), 'Kurfürstendamm 45, 10719, Berlin, Germany', '+491708009466', 'EXPRESS', 'DELIVERED', current_timestamp()),
+       (3, 3, current_timestamp(), 'Karl-Marx-Allee 50, 10243, Berlin, Germany', '+491678019468', 'STANDARD', 'CANCELED', current_timestamp()),
+       (4, 7, current_timestamp(), 'Schlossstasse 45, 10719, Berlin, Germany', '+491707709466', 'STANDARD', 'ON THE WAY', current_timestamp()),
+       (5, 8, current_timestamp(), 'Karslruherstrasse 11, 10243, Schwetzingen, Germany', '+491678044468', 'EXPRESS', 'PENDING', current_timestamp());
 
 insert into order_items (order_item_id, order_id, product_id, quantity, price_at_purchase)
 values (1, 1, 6, 5, 11.05),
