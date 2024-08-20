57a77e4a7b64807ff0f9f87315b8e4353a90de0b
VoytovychM <mary.voytovych@gmail.com>
2024-08-20 11:14:53 +0200

    Update Category.java

--- src/main/java/de/telran/onlineshopforhomeandgarden1/entity/Category.java
+++ src/main/java/de/telran/onlineshopforhomeandgarden1/entity/Category.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Category {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     @Column(name = "category_id")
     private Long id;
 
