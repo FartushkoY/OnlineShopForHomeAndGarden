9c4fc746fc3fce73d7bcef5cc98f6310f2a9ac08
VoytovychM <mary.voytovych@gmail.com>
2024-08-26 16:56:25 +0200

    SCRUM - 61 Add trigger that changes the status of Delivery in the database to the next status every 30 seconds
    - add class DeliveryStatusScheduler and method scheduledDeliveryStatus
    - add method findOrdersRequiringStatusUpdate and query to it;

--- src/main/java/de/telran/onlineshopforhomeandgarden1/repository/OrderRepository.java
+++ src/main/java/de/telran/onlineshopforhomeandgarden1/repository/OrderRepository.java
@@ -2,11 +2,18 @@ package de.telran.onlineshopforhomeandgarden1.repository;
 
 import de.telran.onlineshopforhomeandgarden1.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Set;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     Set<Order> findOrdersByUserId(Long userId);
+
+    @Query(("SELECT o FROM Order o WHERE o.status IN ('PENDING', 'PAID', 'ON_THE_WAY')"))
+    Set<Order> findOrdersRequiringStatusUpdate();
+
+
 
 
 
