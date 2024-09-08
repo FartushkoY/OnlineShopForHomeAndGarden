ffc7b27ac5e177727afccd5f9e7e6a31fca58d7e
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-09-09 00:22:52 +0200

    SCRUM-78 Spring security, SCRUM-26 Realisation method and test authentificationUser

--- src/main/java/de/telran/onlineshopforhomeandgarden1/service/UserService.java
+++ src/main/java/de/telran/onlineshopforhomeandgarden1/service/UserService.java
@@ -8,6 +8,7 @@ import de.telran.onlineshopforhomeandgarden1.repository.UserRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,20 +19,25 @@ public class UserService {
     private static final Logger logger = LogManager.getLogger(UserService.class);
     private final UserRepository repository;
     private final UserMapper mapper;
+    private final PasswordEncoder encoder;
 
     @Autowired
-    public UserService(UserRepository repository, UserMapper mapper) {
+    public UserService(UserRepository repository, UserMapper mapper, PasswordEncoder encoder) {
         this.repository = repository;
         this.mapper = mapper;
+        this.encoder = encoder;
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return repository.findByEmail(email);
     }
 
     public UserRequestDto saveUser(UserRequestDto userRequestDto) {
-        if (userRequestDto.getRole() == null) {
-            userRequestDto.setRole(String.valueOf(Role.CUSTOMER));
-        }
-
         User user = mapper.requestDtoToEntity(userRequestDto);
-
+        if (user.getRole() == null) {
+            user.setRole(Role.CUSTOMER);
+        }
+        user.setPasswordHash(encoder.encode(user.getPasswordHash()));
         User saved = repository.save(user);
         logger.debug("User with id = {} created", saved.getId());
         return mapper.entityToRequestDto(saved);
