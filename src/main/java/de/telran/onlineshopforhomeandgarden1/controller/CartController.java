96f30ec2229b19a847151c83f374bb14ad646f95
IrynaKosiak7 <151627295+IrynaKosiak7@users.noreply.github.com>
2024-09-09 11:01:31 +0200

    [SCRUM-44 added methods and test for Cart], [Scrum-51 added logger Cart] (#56)
    * SCRUM-44 SCRUM-51 added methods and loggers for cart
    
    * updated Cart
    
    * SCRUM-44 added methods and tests for Cart SCRUM-51 logger for Cart
    
    * SCRUM-44 SCRUM-51 added methods and loggers for cart
    
    * SCRUM-44 updated Cart

--- src/main/java/de/telran/onlineshopforhomeandgarden1/controller/CartController.java
+++ src/main/java/de/telran/onlineshopforhomeandgarden1/controller/CartController.java
@@ -1,14 +1,21 @@
 package de.telran.onlineshopforhomeandgarden1.controller;
 
+import de.telran.onlineshopforhomeandgarden1.dto.request.CartItemRequestDto;
+import de.telran.onlineshopforhomeandgarden1.dto.request.CartRequestDto;
+import de.telran.onlineshopforhomeandgarden1.dto.response.CartResponseDto;
 import de.telran.onlineshopforhomeandgarden1.service.CartService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
-@RequestMapping("/carts")
+@RequestMapping("/cart")
 @Validated
 @Slf4j
 public class CartController {
@@ -17,5 +24,36 @@ public class CartController {
     @Autowired
     public CartController(CartService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Set<CartResponseDto>> getItemsInCart() {
+        return new ResponseEntity<>(service.getCartItems(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<CartRequestDto> addItemToCart(@RequestBody @Valid CartItemRequestDto cartItemRequestDto) {
+        CartRequestDto result = service.addCartItem(cartItemRequestDto);
+        if (result != null) {
+            return new ResponseEntity<>(result, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<CartRequestDto> updateCartItem(@RequestBody @Valid CartItemRequestDto cartItemRequestDto) {
+        CartRequestDto cart = service.updateCartItemInCart(cartItemRequestDto);
+        if (cart != null) {
+            return new ResponseEntity<>(cart, cart != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>((HttpStatus.BAD_REQUEST));
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteCartItemInCart() {
+        service.deleteCartItemInCart();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/de/telran/onlineshopforhomeandgarden1/repository/CartRepository.java
+++ src/main/java/de/telran/onlineshopforhomeandgarden1/repository/CartRepository.java
@@ -1,10 +1,14 @@
 package de.telran.onlineshopforhomeandgarden1.repository;
 
 import de.telran.onlineshopforhomeandgarden1.entity.Cart;
+import de.telran.onlineshopforhomeandgarden1.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
 
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
+public interface CartRepository extends JpaRepository<Cart,Long> {
+    Cart findByUserId(Long userId);
+    Set<Cart> findCartsByUserId(Long userId);
 }
